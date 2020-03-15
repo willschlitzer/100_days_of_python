b94e5b97ab4fb80127b66c2a2bed4b11c025638a
Will Schlitzer <schlitzer90@gmail.com>
2020-03-15 13:00:47 +0900

    Show .days method; use numbers in string

--- scratch_file.py
+++ scratch_file.py
@@ -1,22 +1,30 @@
 from datetime import datetime, date
 
+
 def showDateInts():
-    '''Function to show that the datetime class can return int values'''
+    """Function to show that the datetime class can return int values"""
     todaydate = datetime.today()
     # Creates int values from the datetime object
     print(todaydate.day)
     print(todaydate.month)
     print(todaydate.year)
 
+
 def createDate():
-    '''Shows how math can be done with date variables'''
+    """Shows how math can be done with date variables"""
     # Assigns date object using only day month year
     todaydate = date.today()
     # Creates a date object
     christmas = date(2020, 12, 25)
     # Completes subtraction between the two dates
-    print(christmas - todaydate)
+    # .days method returns only subtraction using days
+    christmas_days = ((christmas - todaydate).days)
+    if christmas is not todaydate:
+        print("There are still " + str(christmas_days) + " days until Christmas!" )
+    else:
+        print("Today is Christmas!")
 
-#print(datetime.today())
-#showDateInts()
+
+# print(datetime.today())
+# showDateInts()
 createDate()
