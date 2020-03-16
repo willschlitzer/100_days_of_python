94a05e6c9d8b0ebd04a0f32b84ed1979dafe6c9b
Will Schlitzer <schlitzer90@gmail.com>
2020-03-17 07:11:25 +0900

    Complete Pomodoro Timer

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -1,17 +1,10 @@
 from datetime import datetime, timedelta
 from time import sleep
 
-# The length of time assigned to a given task
-task_length = timedelta(minutes=25)
-# The length of the short break
-short_break = timedelta(minutes=5)
-# The length of a long break
-long_break = timedelta(minutes=30)
-# How many pomodoros happen until a long break
-pom_nums = 4
-
 
 def run_timer():
+    # The length of time assigned to a given task
+    task_length = timedelta(minutes=25)
     start = datetime.today()
     pom_count = 0
     while True:
@@ -21,19 +14,27 @@ def run_timer():
             start = datetime.today()
         else:
             if (elapsed.seconds % 60) == 0:
-                remaining_time = (break_length.seconds - break_elapsed.seconds) / 60
+                remaining_time = int((task_length.seconds - elapsed.seconds) / 60)
                 print(str(remaining_time) + " minutes remaining on your task!")
                 sleep(50)
 
 
 def run_break(pom_count):
+    # The length of the short break
+    short_break = timedelta(minutes=5)
+    # The length of a long break
+    long_break = timedelta(minutes=30)
+    # How many pomodoros happen until a long break
+    pom_nums = 4
     if pom_count >= pom_nums:
         break_length = long_break
         pom_count = 0
     else:
         break_length = short_break
         pom_count += 1
+
     break_start = datetime.today()
+    print("Time for a break!")
     while True:
         break_elapsed = datetime.today() - break_start
         if break_elapsed >= break_length:
@@ -41,9 +42,9 @@ def run_break(pom_count):
             return pom_count
         else:
             if (break_elapsed.seconds % 60) == 0:
-                break_remaining_time = (
-                    break_length.seconds - break_elapsed.seconds
-                ) / 60
+                break_remaining_time = int(
+                    (break_length.seconds - break_elapsed.seconds) / 60
+                )
                 print(str(break_remaining_time) + " minutes remaining on your break!")
                 sleep(50)
 
