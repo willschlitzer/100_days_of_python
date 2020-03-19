e9e91d8a9e0af90e361fa25f679c18764ee7b83e
Will Schlitzer <schlitzer90@gmail.com>
2020-03-19 09:08:43 +0900

    Return highest rated directors

--- highest_rated_directors.py
+++ highest_rated_directors.py
@@ -3,15 +3,22 @@ from collections import defaultdict, namedtuple, Counter, deque
 import csv
 # import random
 from urllib.request import urlretrieve
+from statistics import mean
+
 
 urlretrieve(movie_data, movies_csv)
 
 def find_highest(directors):
     cnt = Counter()
+    director_rating = defaultdict(list)
     # Counts the movies for each director by looking at the length of the associated values for a given director
     for director, movies in directors.items():
         if len(movies) >= 4:
-            print(directors[director][2])
+            scores_list = []
+            for movie in directors[director]:
+                scores_list.append(movie.score)
+            director_rating[director] = mean(scores_list)
+    print(Counter(director_rating).most_common(10))
 
 if __name__ == "__main__":
     directors = get_movies_by_director()
