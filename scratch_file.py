d92994c725ac1361f061c8a4785de332941db899
Will Schlitzer <schlitzer90@gmail.com>
2020-03-24 20:19:57 +0900

    Test random number generator with a patch object to return a given value

--- scratch_file.py
+++ scratch_file.py
@@ -1,2 +1,7 @@
+import random
+
 def hello_you(name="Will"):
     return f"Hello {name}"
+
+def get_random_number(start=1, end=20):
+    return random.randint(1, 20)
--- test_scratch_file.py
+++ test_scratch_file.py
@@ -1,4 +1,13 @@
-from scratch_file import hello_you
+from scratch_file import hello_you, get_random_number
+import random
+from unittest.mock import patch
+
 
 def test_hello_you():
     assert hello_you('Mike') == "Hello Mike"
+
+@patch.object(random, 'randint')
+def test_get_random_number(m):
+    m.return_value=12
+    assert get_random_number() == 12
+
