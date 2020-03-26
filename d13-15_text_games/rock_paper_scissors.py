7e5f38d712061bc1670655059d65fffbc51dedbc
Will Schlitzer <schlitzer90@gmail.com>
2020-03-26 18:29:27 +0900

    Create main game functions

--- d13-15_text_games/rock_paper_scissors.py
+++ d13-15_text_games/rock_paper_scissors.py
@@ -1,1 +1,15 @@
 from rps_classes import Player, Roll
+import random
+
+BEST_OF = 3
+
+def main():
+    print("Time for some Rock, Paper Scissors!")
+    print()
+    game_loop()
+
+def game_loop():
+    pass
+
+if __name__ == "__main__":
+    main()
