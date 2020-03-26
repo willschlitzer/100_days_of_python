1f71ce8b4e8523b6b521787381e0fd57c6efd2c1
Will Schlitzer <schlitzer90@gmail.com>
2020-03-26 13:35:39 +0900

    Create actors.py

---
+++ d13-15_text_games/rpg_game/actors.py
@@ -0,0 +1,30 @@
+import random
+
+class Creature:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    def defensive_roll(self):
+        roll = random.randint(1,12)
+        return roll * self.level
+
+class Dragon(Creature):
+    def __init__(self, name, level, scaliness, breaths_fire):
+        super().__init__(name, level)
+        self.scaliness = scaliness
+        self.breaths_fire = breaths_fire
+
+    def defensive_roll(self):
+        roll = super().defensive_roll()
+        value =  roll * self.scaliness
+        if self.breaths_fire:
+            value = value * 2
+        return value
+
+class Wizard(Creature):
+    def attack(self, creature):
+        my_roll = self.defensive_roll()
+        their_roll = creature.defensive_roll()
+        # Uses Boolean to show who won or lost
+        return my_roll >= their_roll
