0e8f8dc6dbf027db3134f2207572785aef753b3a
Will Schlitzer <schlitzer90@gmail.com>
2020-04-01 10:57:10 +0900

    Create anagram_finder.py

---
+++ d19-21_itertools/anagram_finder.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+
+wordfile = "words_alpha.txt"
+
+with open(wordfile, "r") as f:
+    wordlist = f.read().splitlines()
+
+
+def find_anagrams(word, wordlist=wordlist):
+    word_perms = list(permutations(word))
+    anagram_list = []
+    for perm in word_perms:
+        perm_word = "".join(perm).lower()
+        if (
+            (perm_word in wordlist)
+            and (perm_word not in anagram_list)
+            and (perm_word != word)
+        ):
+            anagram_list.append(perm_word)
+    print(anagram_list)
+
+
+word = "pale"
+find_anagrams(word=word)
--- d19-21_itertools/scratch_file.py
+++ d19-21_itertools/scratch_file.py
@@ -2,12 +2,14 @@ import itertools
 import time
 import sys
 
+
 def spinner():
-    symbols = itertools.cycle('-\|/')
+    symbols = itertools.cycle("-\|/")
     while True:
-        sys.stdout.write('\r' + next(symbols))
+        sys.stdout.write("\r" + next(symbols))
         sys.stdout.flush()
         time.sleep(0.1)
+
 
 def product():
     total_combos = 0
@@ -16,12 +18,14 @@ def product():
         total_combos += 1
     print(total_combos)
 
+
 def perm_com():
     people = "will nick nate".split()
     print(list(itertools.combinations(people, r=2)))
     print(list(itertools.permutations(people, r=3)))
 
+
 if __name__ == "__main__":
-    #spinner()
-    #product()
+    # spinner()
+    # product()
     perm_com()
