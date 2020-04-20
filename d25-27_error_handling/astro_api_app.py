59bed6d801ca78d1531f663030d208c21d6351fa
Will Schlitzer <schlitzer90@gmail.com>
2020-04-20 21:19:36 +0900

    Finish initial script

--- d25-27_error_handling/astro_api_app.py
+++ d25-27_error_handling/astro_api_app.py
@@ -3,12 +3,35 @@
 import requests
 
 
-def astros():
+def astros_query():
     """Queries the API"""
     api_url = "http://api.open-notify.org/astros.json"
-    astros_data = requests.get(api_url).json()
-    return astros_data
+    try:
+        astros_data = requests.get(api_url).json()
+        return astros_data
+    except requests.exceptions.ConnectionError:
+        print("No network connection with API")
+
+def astros_name_parse(astro_data):
+    astronauts = astro_data['people']
+    astro_list = []
+    for astronaut in astronauts:
+        name = astronaut['name']
+        astro_list.append(name)
+    return astro_list
+
+def search_names(name_list):
+    name_search = input("What is the name you are searching? ")
+    name_in_list = False
+    for name in name_list:
+        if name_search in name:
+            print(name)
+            name_in_list = True
+    if name_in_list == False:
+        print("That astronaut is not in space")
 
 if __name__ == "__main__":
-    astros = astros()
-    print(astros)
+    astros = astros_query()
+    astro_list = astros_name_parse(astro_data=astros)
+    search_names(name_list=astro_list)
+
